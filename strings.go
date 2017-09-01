b75f14c52ff240645a53fefa2bac736a939ac979
Gerard Braad <me@gbraad.nl>
2017-09-01 21:00:35 +0800

    Initial commit

---
+++ strings.go
@@ -0,0 +1,73 @@
+/*
+Copyright (C) 2017 Gerard Braad <me@gbraad.nl>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"regexp"
+	"strings"
+)
+
+const (
+	lettersMatch       = "[a-zA-Z]+"
+	numbersMatch       = "[0-9]+"
+	signedNumbersMatch = "^[-+]?[0-9]+"
+)
+
+func checkForMatch(matcher string, strValue string) bool {
+	match := regexp.MustCompile(matcher)
+	return match.FindString(strValue) != ""
+}
+
+// HasLetters returns true when string contains a letter [a-zA-Z]
+func HasLetters(yourString string) bool {
+	return checkForMatch(lettersMatch, yourString)
+}
+
+// HasOnlyLetters returns true when string contains only letters
+func HasOnlyLetters(yourString string) bool {
+	return checkForMatch(lettersMatch, yourString) &&
+		!checkForMatch(numbersMatch, yourString)
+}
+
+// HasNumbers returns true when string contains a letter [0-9]
+func HasNumbers(yourString string) bool {
+	return checkForMatch(numbersMatch, yourString)
+}
+
+// HasOnlyNumbers returns true when string contains only numbers
+func HasOnlyNumbers(yourString string) bool {
+	return checkForMatch(numbersMatch, yourString) &&
+		!checkForMatch(lettersMatch, yourString)
+}
+
+func getOnlyMatch(matcher string, strValue string) string {
+	reg, _ := regexp.Compile(matcher)
+	return reg.FindString(strValue)
+}
+
+// GetOnlyLetters returns a string containing only letters from given string
+func GetOnlyLetters(yourString string) string {
+	return getOnlyMatch(lettersMatch, yourString)
+}
+
+// GetOnlyNumbers returns a string containing only numbers from given string
+func GetOnlyNumbers(yourString string) string {
+	return getOnlyMatch(numbersMatch, yourString)
+}
+
+// GetSignedNumbers returns a string containing only positive and negative numbers from given string
+func GetSignedNumbers(yourString string) string {
+	return getOnlyMatch(signedNumbersMatch, yourString)
+}
---
+++ strings_test.go
@@ -0,0 +1,81 @@
+/*
+Copyright 2017 Gerard Braad <me@gbraad.nl>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package strings
+
+import (
+	"reflect"
+	"runtime"
+	"testing"
+)
+
+func getFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func TestHasMatcher(t *testing.T) {
+	var testCases = []struct {
+		testString     string
+		matcher        func(string) bool
+		expectedResult bool
+	}{
+		{"abc", HasLetters, true},
+		{"123", HasNumbers, true},
+		{"abc", HasNumbers, false},
+		{"123", HasLetters, false},
+		{"abc123", HasLetters, true},
+		{"abc123", HasNumbers, true},
+		{"abc", HasOnlyLetters, true},
+		{"123", HasOnlyNumbers, true},
+		{"abc123", HasOnlyLetters, false},
+		{"abc123", HasOnlyNumbers, false},
+		{"!@#$%^&*()", HasLetters, false},
+		{"!@#$%^&*()", HasNumbers, false},
+		{"!@#$%^&*()", HasOnlyLetters, false},
+		{"!@#$%^&*()", HasOnlyNumbers, false},
+	}
+
+	for _, testCase := range testCases {
+		actualResult := testCase.matcher(testCase.testString)
+		if actualResult != testCase.expectedResult {
+			t.Errorf("Unexpected result for '%s'. Expected '%s' to return '%t', but got '%t'", getFunctionName(testCase.matcher), testCase.testString, testCase.expectedResult, actualResult)
+		}
+	}
+}
+
+func TestGetMatcher(t *testing.T) {
+	var testCases = []struct {
+		testString     string
+		matcher        func(string) string
+		expectedResult string
+	}{
+		{"abc", GetOnlyLetters, "abc"},
+		{"123", GetOnlyNumbers, "123"},
+		{"abc123", GetOnlyLetters, "abc"},
+		{"abc123", GetOnlyNumbers, "123"},
+		{"!@#$%^&*()", GetOnlyLetters, ""},
+		{"!@#$%^&*()", GetOnlyNumbers, ""},
+		{"123GB", GetSignedNumbers, "123"},
+		{"-123GB", GetSignedNumbers, "-123"},
+	}
+
+	for _, testCase := range testCases {
+		actualResult := testCase.matcher(testCase.testString)
+		if actualResult != testCase.expectedResult {
+			t.Errorf("Unexpected result for '%s'. Expected '%s' to return '%t', but got '%t'", getFunctionName(testCase.matcher), testCase.testString, testCase.expectedResult, actualResult)
+		}
+	}
+}
